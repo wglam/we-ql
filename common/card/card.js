4198cda5cf43f32745abcdabc89d17d7d9819998
wglam <1209808782@qq.com>
2018-09-22 10:05:47 +0800

    6

--- common/card/card.js
+++ common/card/card.js
@@ -4,20 +4,31 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    url:String
+    url:String,
+    title:String,
+    info:String,
+    btn:String,
+    progress:{
+      type: Number,
+      value:-1
+    },
+    progressName:String,
+    bottom:Boolean
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+   
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onClick:function(){
+      this.triggerEvent('Click')
+    }
   }
 })
