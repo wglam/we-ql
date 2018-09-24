55e940875971cdc83f875e4e630c44183c0b7cee
wglam <1209808782@qq.com>
2018-09-24 10:54:54 +0800

    1

---
+++ common/vip/buyinfo.js
@@ -0,0 +1,23 @@
+// plugins/vip/buyinfo.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    vip:Boolean
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
