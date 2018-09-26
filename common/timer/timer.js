a6dc68b61aee79a064c58aedd2a292ff7f8f77c5
wglam <1209808782@qq.com>
2018-09-26 21:13:11 +0800

    拼团

--- common/timer/timer.js
+++ common/timer/timer.js
@@ -4,12 +4,31 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    before:String,
+    after:String,
     time: {
       type: Number,
       value: 0,
       observer: function(newVal, oldVal, changedPath) {
         // 属性被改变时执行的函数（可选），也可以写成在methods段中定义的方法名字符串, 如：'_propertyChange'
         // 通常 newVal 就是新设置的数据， oldVal 是旧数据
+        // var imilliseconds = newVal % 1000;
+        var iseconds = parseInt(newVal / 1000)
+        var iminute = parseInt(iseconds / 60);
+        var ihour = parseInt(iminute / 60);
+        iminute = iminute % 60;
+        iseconds = iseconds % 60;
+        // imilliseconds = parseInt(imilliseconds / 100);
+
+        var data = {
+          hour: this.formatTimer(ihour),
+          minute: this.formatTimer(iminute),
+          seconds: this.formatTimer(iseconds),
+          // milliseconds: imilliseconds
+        }
+
+        var that = this;
+        that.setData(data);
       }
     }
   },
@@ -21,13 +40,16 @@ Component({
     hour: "00",
     minute: "00",
     seconds: "00",
-    milliseconds: "00"
+    // milliseconds: "0"
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    formatTimer: function(v) {
+      var a = v < 10 ? "0" + v : v
+      return a;
+    }
   }
 })
--- pages/vip/group/group.js
+++ pages/vip/group/group.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    nowDate:new Date(),
+    nowDate: new Date(),
     swiper: [{
       video: 'https://pic.ibaotu.com/00/78/86/31w888piC8Pc.mp4',
       image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1537262844254&di=78e033e0836ad431ead59adb7e5a6464&imgtype=0&src=http%3A%2F%2Fpic.35pic.com%2Fnormal%2F08%2F35%2F11%2F3637404_163333224000_2.jpg'
@@ -17,16 +17,17 @@ Page({
       image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1537262850940&di=19c04f38fb61236f689b6d7b9b120574&imgtype=0&src=http%3A%2F%2Fimg02.tooopen.com%2Fproducts%2F20150425%2Ftooopen_89347611.jpg'
     }],
     price: 15.8,
+    selfPrice:25.8,
     srcPrice: 128,
     tips: '已拼457815件',
     title: '普通会员 1个月',
-    num: 44,
+    groupnum: 44,
     groups: [{
         name: "张三丰",
         avatar: "http://image.weilanwl.com/img/square-3.jpg",
         sum: 2,
         current: 1,
-        deadline: '',
+        deadline: 1538222566000,
         color: '#f0f'
       },
       {
@@ -34,53 +35,57 @@ Page({
         avatar: "http://image.weilanwl.com/img/square-3.jpg",
         sum: 2,
         current: 1,
-        deadline: '',
+        deadline: 1538222566000,
         color: '#f0f'
       }, {
         name: "张三丰3",
         avatar: "http://image.weilanwl.com/img/square-3.jpg",
         sum: 2,
         current: 1,
-        deadline: '',
+        deadline: 1538222566000,
         color: '#f0f'
       }, {
         name: "张三丰4",
         avatar: "http://image.weilanwl.com/img/square-3.jpg",
         sum: 2,
         current: 1,
-        deadline: '',
+        deadline: 1538222566000,
         color: '#f0f'
       }, {
         name: "张三丰5",
         avatar: "http://image.weilanwl.com/img/square-3.jpg",
         sum: 2,
         current: 1,
-        deadline: '',
+        deadline: 1538222566000,
         color: '#f0f'
       }
-    ]
+    ],
+    currentTime: 0,
+    
   },
   startInterval: function() {
-    varthat = this;
-    setInterthat.data.setInter = setInterval(function() {
-      varnumVal = that.data.num + 1;
+    var that = this;
+    var numtime = new Date().getTime();
+    that.setData({
+      currentTime: numtime
+    });
+    that.data.setInter = setInterval(function() {
+      var numVal = new Date().getTime();
       that.setData({
-        num: numVal
+        currentTime: numVal
       });
-      console.log('setInterval==' + that.data.num);
-    }, 2000);
+    }, 199);
   },
   endSetInter: function() {
     varthat = this;
     clearInterval(that.data.setInter)
   },
-  onHide: function() {},
-  onUnload: function() {
+  onShow: function() {
+    var that = this;
+    that.startInterval();
+  },
+  onHide: function() {
     varthat = this;
     clearInterval(that.data.setInter)
-  },
-
-
-
-
+  }
 })
