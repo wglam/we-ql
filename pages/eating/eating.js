b676eba3fae93ced2ca7c53de8f02b4892e7fc40
wglam <1209808782@qq.com>
2018-09-28 21:12:28 +0800

    饮食打卡

--- pages/eating/eating.js
+++ pages/eating/eating.js
@@ -5,62 +5,87 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    colors: ["#A6CBA0", "#ebb774", "#FF856E"],
+    items: [{
+      title: "补水情况",
+      ttips: "运动中补水：",
+      tips: "可以随时小口补水，但是不要一下子喝太多;",
+      texamples: "平时补水：",
+      examples: "每天8杯水为好，大概在1.5-2升，一天均匀补水"
+    }, {
+      title: "早餐推荐",
+      ttips: "饮食结构：",
+      tips: "鸡蛋白1-2个 脂肪 坚果20g 小番茄5个",
+      texamples: "搭配示例：",
+      examples: "鸡蛋白1-2个脂肪坚果20g小番茄5个鸡蛋白1-2个脂肪坚果20g小番茄5个鸡蛋白1-2个脂肪坚果20g小番茄5个"
+    }, {
+      title: "中餐推荐",
+      ttips: "饮食结构：",
+      tips: "鸡蛋白1-2个 脂肪 坚果20g 小番茄5个",
+      texamples: "搭配示例：",
+      examples: "鸡蛋白1-2个脂肪坚果20g小番茄5个鸡蛋白1-2个脂肪坚果20g小番茄5个鸡蛋白1-2个脂肪坚果20g小番茄5个"
+    }, {
+      title: " 晚餐推荐",
+      ttips: "饮食结构：",
+      tips: "鸡蛋白1-2个 脂肪 坚果20g 小番茄5个",
+      texamples: "搭配示例：",
+      examples: "鸡蛋白1-2个脂肪坚果20g小番茄5个鸡蛋白1-2个脂肪坚果20g小番茄5个鸡蛋白1-2个脂肪坚果20g小番茄5个"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
