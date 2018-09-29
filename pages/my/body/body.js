fd0d2d3196357fc87887ee4734d7a104d0ca3954
wglam <1209808782@qq.com>
2018-09-29 17:07:39 +0800

    1

---
+++ pages/my/body/body.js
@@ -0,0 +1,72 @@
+// pages/my/body/body.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    weight: 64.0,
+    height: 170,
+    bmi: 24,
+    xiongwei: 0,
+    yaowei: 0,
+    tuiwei: 0,
+    bmiName:"过重"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
