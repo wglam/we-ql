aedd98763bad07ee33e079632c2fe4210e4bbacd
wglam <1209808782@qq.com>
2018-09-29 10:49:13 +0800

    1

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,62 +5,69 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    user: {
+      name: "张三丰",
+      avatar: "http://image.weilanwl.com/img/square-4.jpg",
+      vip: true,
+      vipname: '普通会员',
+      flag: "自我打败是最可悲的失败，自我战胜是最可贵的胜利。",
+      day:20
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
