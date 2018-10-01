1f4cc69a7ca4523704311e5a5480fb7ae3cf3d0b
wglam <1209808782@qq.com>
2018-10-01 10:04:04 +0800

    饮食相册

---
+++ pages/my/photo/photo.js
@@ -0,0 +1,22 @@
+// pages/my/photo/photo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    photos: [{
+      time: '2018-10-01',
+      photo: 'http://image.weilanwl.com/img/square-4.jpg'
+    }, {
+      time: '2018-9-30',
+      photo: 'http://image.weilanwl.com/img/square-3.jpg'
+    }, {
+      time: '2018-9-25',
+      photo: 'http://image.weilanwl.com/img/square-2.jpg'
+    }, {
+      time: '2018-7-10',
+      photo: 'http://image.weilanwl.com/img/square-1.jpg'
+    }]
+  },
+})
