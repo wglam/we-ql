e0bd8131f86559df900213b9cb54e1e3c7d3979f
wglam <1209808782@qq.com>
2018-10-01 17:02:31 +0800

    1

---
+++ pages/my/coach/coach.js
@@ -0,0 +1,14 @@
+// pages/my/coach/coach.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: "张三丰",
+    avatar: "http://image.weilanwl.com/img/square-4.jpg",
+    qr:'qr.jpg'
+  },
+
+
+})
