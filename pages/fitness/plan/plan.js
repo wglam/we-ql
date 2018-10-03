feb7ee5c133f336226f207802d7d11d99d263142
wglam <1209808782@qq.com>
2018-10-03 11:54:51 +0800

    训练计划

--- pages/fitness/plan/plan.js
+++ pages/fitness/plan/plan.js
@@ -5,62 +5,109 @@ Page({
    * 页面的初始数据
    */
   data: {
+    top: 0,
+    current: 0,
+    items: [{
+      name: '热身',
+      items: [{
+        target: '热身动作',
+        name: '开合跳',
+        zushu: 10,
+        cishu: 15,
+        video: 'xxxx',
+        rest: "15s",
+        value: 0
+      }]
+    }, {
+      name: '训练',
+      items: [{
+
+        target: '胸部',
+        name: '跪姿俯卧撑',
+        zushu: 10,
+        cishu: -1,
+        rest: "30s",
+        value: 0
+      }, {
+
+        target: '肩部',
+        name: '哑铃侧平举',
+        zushu: 4,
+        cishu: 20,
+        rest: "30s",
+        value: 0
+      }]
+    }, {
+      name: '有氧',
+      items: [{
+        name: '快走',
+        time: "40分钟",
+        xinlv: "130次/分",
+        strong: 1,
+        value: 0
+      }]
+    }, {
+      name: '拉伸',
+      items: [{
+        target: "拉伸",
+        name: '四头肌拉伸',
+        zushu: 3,
+        time: "30s",
+        value: 0
+      }]
+    }],
+    inputHide: true,
+    slider: {
+      unit: "组",
+      max: 10,
+      value: 0,
+      top: 0,
+      index: 0
+    }
+  },
+  complete: function(e) {
+    var that = this;
+    var val = that.data.slider
+    val.value = e.currentTarget.dataset.item.value
+    val.top = e.currentTarget.dataset.top
+    val.index = e.currentTarget.dataset.index
+    that.setData({
+      slider: val,
+      inputHide: false
+    });
+
+    // console.log(e.currentTarget.dataset.top);
+    // console.log(e.currentTarget.dataset.index);
+    console.log(that.data.sliderItem);
+  },
+  _hideInput: function() {
+    var that = this;
+    that.setData({
+      inputHide: true
+    });
+  },
+  confirm: function(e) {
+    console.log(e.detail.value);
+  },
+  cancel: function(e) {
+    this._hideInput();
+  },
+  empty: function(e) {
 
   },
+  sliderChange: function(e) {
+    var that = this;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    //
+    var val = that.data.slider
+    val.value = e.detail.value
 
-  },
+    var vals = that.data.items;
+    vals[val.top].items[val.index].value = e.detail.value
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    that.setData({
+      items: vals,
+      slider: val
+    })
   }
 })
