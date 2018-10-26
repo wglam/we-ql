1e9fedc325b1573c7f9d717d448befd35ecd42e6
wglam <1209808782@qq.com>
2018-10-26 13:45:54 +0800

    1

--- pages/home/home.js
+++ pages/home/home.js
@@ -176,7 +176,7 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function(ops) {
     var self = this;
     if (ops.from === 'menu') {
       var shareObj = {
