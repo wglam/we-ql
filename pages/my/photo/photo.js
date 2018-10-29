3247ba7063499e8cd3eb3df11ee81f67f6c880a0
wglam <1209808782@qq.com>
2018-10-29 14:28:34 +0800

    1

--- pages/my/photo/photo.js
+++ pages/my/photo/photo.js
@@ -33,5 +33,75 @@ Page({
         console.log(e)
         wx.hideLoading()
       })
-  }
+  },
+  submitDiet: function () {
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        that.uploadDietFile(res.tempFilePaths[0]);
+      }
+    })
+  },
+  uploadDietFile: function (f) {
+    wx.showLoading({
+      title: '正在提交',
+    })
+    g.api.uploadFile(f,
+      res => {
+        console.log(res)
+        var val = JSON.parse(res.data)
+        if (val.retCode == "0000") {
+          var pic = val.list[0]
+          g.api.addMemberDietPlan({
+            data: {
+              memberDietPlan: {
+                imgStr: pic,
+                memberId: g.userInfo.memberId
+              }
+            }
+          })
+            .then(res => {
+              wx.hideLoading()
+              if (res.data.retCode == '0000') {
+                wx.navigateTo({
+                  url: '/pages/my/photo/photo',
+                })
+                wx.showToast({
+                  title: '打卡成功'
+                });
+              } else {
+                wx.showToast({
+                  title: '打卡失败',
+                  icon: 'none'
+                });
+              }
+
+            })
+            .catch(res => {
+              wx.hideLoading()
+              wx.showToast({
+                title: '打卡失败',
+                icon: 'none'
+              });
+
+            })
+        } else {
+          wx.hideLoading()
+          wx.showToast({
+            title: '打卡失败',
+            icon: 'none'
+          });
+        }
+      },
+      fail => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '打卡失败，请重试'
+        });
+      })
+  },
 })
